af95339c156b02bc6370ddb1d75d6429c9bf8bb3
21givenchy <65071041+21givenchy@users.noreply.github.com>
2023-04-18 12:21:47 +0000

    first mvp dont look like shit

--- frontforumfocus/app/page.tsx
+++ frontforumfocus/app/page.tsx
@@ -1,33 +1,17 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useState } from 'react'
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  const [count, setCount] = useState(0);
-
+const HomePage: React.FC = () => {
   return (
-    <div style={{ fontFamily: inter }}>
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Community Initiatives</a></li>
-          <li><a href="#">Marketplace</a></li>
-          <li><a href="#">Educational Resources</a></li>
-        </ul>
-      </nav>
-      <main>
-        <h1>Frontforumfocus</h1>
-        <p>Welcome to Frontforumfocus, a platform that connects individuals and organizations working on sustainable development goals through community initiatives, marketplace and educational resources.</p>
-        <div>
-          <button onClick={() => setCount(count + 1)}>Click me</button>
-          <p>You've clicked the button {count} times!</p>
-        </div>
-      </main>
-      <footer>
-        <p>&copy; 2022 Frontforumfocus. All rights reserved.</p>
-      </footer>
-    </div>
+    <>
+      <h1 style={{ fontSize: '48px', fontWeight: 'bold', textAlign: 'center', marginTop: '40px' }}>FrontForumFocus</h1>
+      <p style={{ textAlign: 'center', fontSize: '20px', lineHeight: '1.5', marginBottom: '40px' }}>
+        A platform connecting individuals and organizations working on sustainable development goals through community initiatives, marketplace, and educational resources
+      </p>
+      <button style={{ display: 'block', margin: '0 auto', fontSize: '20px', padding: '10px 20px', borderRadius: '8px', backgroundColor: '#0070f3', color: '#fff', border: 'none' }}>
+        Get Started
+      </button>
+    </>
   );
-}
+};
+
+export default HomePage;
---
+++ frontforumfocus/components/Container.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+export default Container;
